e4ebe38d5c6bbdbacc755fd3711e87cb36cc12bd
SaltyEgg <hzhou.me@saltyee.com>
2024-10-03 00:26:30 -0700

    use random number now

--- main/hello-world.cc
+++ main/hello-world.cc
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <random>
 #include <string>
 
 #include "lib/hello-time.h"
 #include "main/guess-number.h"
 #include "main/hello-greet.h"
+
+int generate_random_number() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(1, 100);
+  return dis(gen);
+}
 
 int main(int argc, char** argv) {
   std::string who = "world";
@@ -13,6 +21,6 @@ int main(int argc, char** argv) {
   std::cout << get_greet(who) << std::endl;
   print_localtime();
 
-  guess_number_with_clues(42);
+  guess_number_with_clues(generate_random_number());
   return 0;
 }
