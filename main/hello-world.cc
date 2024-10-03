4429ea1810c2279093fe5a0ea1549dd799c14f75
Hao Zhou <hzhou.me@gmail.com>
2024-10-03 13:58:15 -0700

    demo for copy constructor

--- main/hello-world.cc
+++ main/hello-world.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <random>
 #include <string>
 
@@ -13,14 +14,49 @@ int generate_random_number() {
   return dis(gen);
 }
 
-int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) {
-    who = argv[1];
+class MyClass {
+ public:
+  MyClass(int v) : value(v) {
+    std::cout << "Constructor called with value " << value << std::endl;
   }
-  std::cout << get_greet(who) << std::endl;
-  print_localtime();
 
-  guess_number_with_clues(generate_random_number());
+  MyClass(const MyClass& other) : value(other.value) {
+    std::cout << "Copy constructor called with value " << value << std::endl;
+  }
+
+  ~MyClass() {
+    std::cout << "Destructor called for value " << value << std::endl;
+  }
+
+  int getValue() const { return value; }
+
+ private:
+  int value;
+};
+
+void refCopy(const MyClass& original) {
+  auto sharedPtr = std::make_shared<const MyClass>(original);
+  std::cout << '\t' << sharedPtr.get()->getValue() << std::endl;
+}
+
+int main() {
+  MyClass original(42);
+  std::cout << "Address of original: " << &original << std::endl;
+
+  auto sharedPtr = std::make_shared<const MyClass>(original);
+  std::cout << "Address of shared object: " << sharedPtr.get() << std::endl;
+  std::cout << "Value in shared object: " << sharedPtr->getValue() << std::endl
+            << std::endl;
+
+  refCopy(original);
+
+  // std::string who = "world";
+  // if (argc > 1) {
+  //   who = argv[1];
+  // }
+  // std::cout << get_greet(who) << std::endl;
+  // print_localtime();
+
+  // guess_number_with_clues(generate_random_number());
   return 0;
 }
